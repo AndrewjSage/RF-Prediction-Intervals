5daadf88decc82af9484b50c0a19482e67c372b1
andrewjsage <ajsage@gmail.com>
2021-08-06 11:02:07 -0500

    fixed errors in PI's in real data app

--- Real_Data_App.R
+++ Real_Data_App.R
@@ -18,8 +18,19 @@ library(gridExtra)
 #x <- read.delim("https://raw.githubusercontent.com/haozhestat/RFIntervals/master/DataAnalysis/data/nipsdata/Boston/x.txt", header=FALSE, sep=" ")
 #y <- read.delim("https://raw.githubusercontent.com/haozhestat/RFIntervals/master/DataAnalysis/data/nipsdata/Boston/y.txt", header=FALSE, sep=" ")
 #Insurance
-x <- read.delim("https://raw.githubusercontent.com/haozhestat/RFIntervals/master/DataAnalysis/data/nipsdata/Insur/x.txt", header=FALSE, sep=" ")
-y <- read.delim("https://raw.githubusercontent.com/haozhestat/RFIntervals/master/DataAnalysis/data/nipsdata/Insur/y.txt", header=FALSE, sep=" ")
+#x <- read.delim("https://raw.githubusercontent.com/haozhestat/RFIntervals/master/DataAnalysis/data/nipsdata/Insur/x.txt", header=FALSE, sep=" ")
+#y <- read.delim("https://raw.githubusercontent.com/haozhestat/RFIntervals/master/DataAnalysis/data/nipsdata/Insur/y.txt", header=FALSE, sep=" ")
+#Attend
+#x <- read.delim("https://raw.githubusercontent.com/haozhestat/RFIntervals/master/DataAnalysis/data/nipsdata/Attend/x.txt", header=FALSE, sep=" ")
+#y <- read.delim("https://raw.githubusercontent.com/haozhestat/RFIntervals/master/DataAnalysis/data/nipsdata/Attend/y.txt", header=FALSE, sep=" ")
+#Baseball
+#x <- read.delim("https://raw.githubusercontent.com/haozhestat/RFIntervals/master/DataAnalysis/data/nipsdata/Baseball/x.txt", header=FALSE, sep=" ")
+#y <- read.delim("https://raw.githubusercontent.com/haozhestat/RFIntervals/master/DataAnalysis/data/nipsdata/Baseball/y.txt", header=FALSE, sep=" ")
+#Baseball
+x <- read.delim("https://raw.githubusercontent.com/haozhestat/RFIntervals/master/DataAnalysis/data/nipsdata/Budget/x.txt", header=FALSE, sep=" ")
+y <- read.delim("https://raw.githubusercontent.com/haozhestat/RFIntervals/master/DataAnalysis/data/nipsdata/Budget/y.txt", header=FALSE, sep=" ")
+
+
 
 names(y) <- "y"
 Data <- cbind(x,y)
@@ -38,8 +49,8 @@ ui <- fluidPage(
 fluidRow(
   # Show a plot of the generated distribution
   mainPanel(
-    plotOutput("plot"),
-    textOutput("text")
+    plotOutput("plot")
+    #tableOutput("table")
   )  
 )
 
@@ -75,7 +86,7 @@ server <- function(input, output){
    LM_PI <- predict(LM, newdata=Test, interval="prediction", level=1-alpha) 
    #LM_Contains <- (Test$y >= LM_PI[,2]) & (Test$y <= LM_PI[,3])
    #Cov_LM <- mean(LM_Contains)
-   #Width_LM <- mean(LM_PI[,3]-LM_PI[,2])
+   Width_LM <- mean(LM_PI[,3]-LM_PI[,2])
    
    RF <- quantreg(data=Train, y~., method="forest", prob=c(alpha/2, 1-alpha/2), ntree=100, nodesize=ns)
    RFpredinfo <- quantreg(object=RF, newdata=Test, prob=c(alpha/2, 1-alpha/2))
@@ -88,7 +99,7 @@ server <- function(input, output){
    RF_PI2 <- data.frame(RFpred, RFpred-MOE, RFpred+MOE)
    #RF2_Contains <- (Test$y >= RF_PI2[,2]) & (Test$y <= RF_PI2[,3])
    #Coverage_RFOOB_Sym <- mean(RF2_Contains)
-   #Width_RFOOB_Sym <- mean(RF_PI2[,3]-RF_PI2[,2])
+   Width_RFOOB_Sym <- mean(RF_PI2[,3]-RF_PI2[,2])
    # Intervals from Random Forest - Non. Sym. OOB method
    # Assumes constant variance
    Lower <- quantile(OOB_resid, alpha/2)
@@ -96,7 +107,7 @@ server <- function(input, output){
    RF_PI <- data.frame(RFpred, RFpred+Lower, RFpred+Upper)
    #RF_Contains <- (Test$y >= RF_PI[,2]) & (Test$y <= RF_PI[,3])
    #Coverage_RFOOB_NonSym <- mean(RF_Contains)
-   #Width_RFOOB_NonSym <- mean(RF_PI[,3]-RF_PI[,2])
+   Width_RFOOB_NonSym <- mean(RF_PI[,3]-RF_PI[,2])
    # Intervals from Quantile Random Forest 
    # Does not assume constant variance
    # QRFLower <- RFpredinfo$quantreg$quantiles[,1]
@@ -104,7 +115,7 @@ server <- function(input, output){
    # QRF_PI <- data.frame(RFpred, QRFLower, QRFUpper)
    # QRF_Contains <- (Test$y >= QRF_PI[,2]) & (Test$y <= QRF_PI[,3])
    # Coverage_QRF <- mean(QRF_Contains)
-   # Width_QRF <- mean(QRF_PI[,3]-QRF_PI[,2])   
+   #Width_QRF <- mean(QRF_PI[,3]-QRF_PI[,2])   
    library(quantregForest)
    Trainx <- Train[,1:(ncol(Train)-1)]
    y <- Train$y
@@ -122,7 +133,7 @@ server <- function(input, output){
    QRF_PI <- data.frame(QRFpred, QRFLower, QRFUpper)
    #QRF_Contains <- (Test$y >= QRF_PI[,2]) & (Test$y <= QRF_PI[,3])
    #Coverage_QRF <- mean(QRF_Contains)
-   #Width_QRF <- mean(QRF_PI[,3]-QRF_PI[,2])   
+   Width_QRF <- mean(QRF_PI[,3]-QRF_PI[,2])   
 
 LM_PI <- data.frame(LM_PI)   
 LM_PI$x <- Test[,varnum]      
@@ -132,14 +143,19 @@ QRF_PI$x <- Test[,varnum]
 names(RF_PI) <- names(RF_PI2) <- names(QRF_PI) <- names(LM_PI)
 
    
-#   Res_LM <- c(MSPE_LM, Cov_LM, Width_LM)
-#   Res_RFOOBSym <- c(MSPE_RF, Coverage_RFOOB_Sym, Width_RFOOB_Sym)
-#   Res_RFOOBNonSym <- c(MSPE_RF, Coverage_RFOOB_NonSym, Width_RFOOB_NonSym)
-#   Res_QRF <- c(MSPE_RF, Coverage_QRF, Width_QRF)
+   #Res_LM <- c(MSPE_LM, Cov_LM, Width_LM)
+   #Res_RFOOBSym <- c(MSPE_RF, Coverage_RFOOB_Sym, Width_RFOOB_Sym)
+   #Res_RFOOBNonSym <- c(MSPE_RF, Coverage_RFOOB_NonSym, Width_RFOOB_NonSym)
+   #Res_QRF <- c(MSPE_RF, Coverage_QRF, Width_QRF)
    
-#   Res_Table <- t(data.frame(Res_LM, Res_RFOOBSym, Res_RFOOBNonSym, Res_QRF))
-#   colnames(Res_Table) <- c("MSPE", "Coverage", "Width")
-#   rownames(Res_Table) <- c("Linear Model", "Random Forest with Symmetry Assumption", "Random Forest - No Symmetry Assumption", "Random Forest - No Constant Variance Assumption")
+  Res_LM <- c(Width_LM)
+  Res_RFOOBSym <- c(Width_RFOOB_Sym)
+  Res_RFOOBNonSym <- c(Width_RFOOB_NonSym)
+  Res_QRF <- c(Width_QRF)
+
+   Res_Table <- t(data.frame(Res_LM, Res_RFOOBSym, Res_RFOOBNonSym, Res_QRF))
+   colnames(Res_Table) <- c("Width")
+   rownames(Res_Table) <- c("Linear Model", "Random Forest with Symmetry Assumption", "Random Forest - No Symmetry Assumption", "Random Forest - No Constant Variance Assumption")
 
    #Plot <- qplot(Test$V1, Test$y)
    p1<-ggplot(data=LM_PI, aes(x=x, y=fit)) + geom_line(aes(x=x, y=LM_PI[,1]), color="red") + ylim(c(1*min(Data$y), 1*max(Data$y)))
@@ -148,38 +164,40 @@ names(RF_PI) <- names(RF_PI2) <- names(QRF_PI) <- names(LM_PI)
    p2<-ggplot(data=RF_PI2, aes(x=x, y=fit)) + geom_line(aes(x=x, y=RF_PI2[,1]), color="red")  + ylim(c(1*min(Data$y), 1*max(Data$y)))
    p2 <- p2+geom_ribbon(aes(ymin=RF_PI2[,2], ymax=RF_PI2[,3]), linetype=2, alpha=0.5) + ggtitle("Assumes Norm., CV") + theme(legend.position = "none")
    
-   p3<-ggplot(data=RF_PI2, aes(x=x, y=fit)) + geom_line(aes(x=x, y=RF_PI[,1]), color="red")  + ylim(c(1*min(Data$y), 1*max(Data$y)))
-   p3<-p3+geom_ribbon(aes(ymin=RF_PI[,2], ymax=RF_PI2[,3]), linetype=2, alpha=0.5) + ggtitle("Assumes CV") + theme(legend.position = "none")
+   p3<-ggplot(data=RF_PI, aes(x=x, y=fit)) + geom_line(aes(x=x, y=RF_PI[,1]), color="red")  + ylim(c(1*min(Data$y), 1*max(Data$y)))
+   p3<-p3+geom_ribbon(aes(ymin=RF_PI[,2], ymax=RF_PI[,3]), linetype=2, alpha=0.5) + ggtitle("Assumes CV") + theme(legend.position = "none")
    
    p4<-ggplot(data=QRF_PI, aes(x=x, y=fit)) + geom_line(aes(x=x, y=QRF_PI[,1]), color="red")  + ylim(c(1*min(Data$y), 1*max(Data$y)))
-   p4<-p4+geom_ribbon(aes(ymin=QRF_PI[,2], ymax=RF_PI2[,3]), linetype=2, alpha=0.5) + ggtitle("Assumes Lin., Norm., CV") + theme(legend.position = "none")
+   p4<-p4+geom_ribbon(aes(ymin=QRF_PI[,2], ymax=QRF_PI[,3]), linetype=2, alpha=0.5) + ggtitle("Assumes Lin., Norm., CV") + theme(legend.position = "none")
    
    LM_Res <- data.frame(Train$y)
    names(LM_Res) <- c("y")
    LM_Res$Predicted <- LM$fitted.values
    LM_Res$Residual <- LM_Res$y - LM_Res$Predicted
+   LM_Res$x <- Train[,varnum]
    ResidPlot <- ggplot(data=LM_Res, aes(x=Predicted, y=Residual)) + geom_point()
    ResidHist <- ggplot(data=LM_Res, aes(x=Residual)) + geom_histogram()
    QQPlot <- ggplot(data=LM_Res, aes(sample = Residual)) + stat_qq() + stat_qq_line() + xlab("Normal Quantiles") + ylab("Residual Quantiles") + ggtitle("QQ Plot")
-   
+  ResidvsPred <- ggplot(data=LM_Res, aes(x=x, y=y)) + geom_point() + stat_smooth()
+  
    #DiagnosticPlots <-  grid.arrange(ResidPlot, ResidHist, QQPlot, ncol=3)
    #PIPlots <- grid.arrange(p1, p2, p3, p4, ncol=2)
    #Plot <- if(Disp == "Linear Model Diagnostics"){DiagnosticPlots}else{PIPlots}
    #Text <- Disp
   
    Plot <- if(Disp == "Linear Model Diagnostics")
-   {grid.arrange(ResidPlot, ResidHist, QQPlot, ncol=3)}else{grid.arrange(p1, p2, p3, p4, ncol=2)}
-   Text <- Disp
+   {grid.arrange(ResidPlot, ResidHist, QQPlot, ResidvsPred, ncol=4)}else{grid.arrange(p1, p2, p3, p4, ncol=2)}
+   Table <- Res_Table
    
    
-   return(list(Plot, Text))
+   return(list(Plot, Table))
    }
 
   
   SimulationRes <- reactive({Simulation(input$var, input$Disp)})
   #SimulationPlt <- reactive({Plottest(input$Disp)})
   output$plot <- renderPlot(SimulationRes()[[1]])
-  output$text <- renderText(SimulationRes()[[2]])
+  output$table <- renderTable(SimulationRes()[[2]])
 }
 
 
