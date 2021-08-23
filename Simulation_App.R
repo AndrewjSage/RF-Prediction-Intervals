163be1081f079663ddc43346ffa20c11df843499
andrewjsage <ajsage@gmail.com>
2021-08-23 14:15:04 -0500

    added checkboxes for PI assumptions

--- Simulation_App.R
+++ Simulation_App.R
@@ -11,7 +11,7 @@
 # Define UI for application that draws a histogram
 library(shiny)
 library(tidyverse)
-library(randomForestSRC)
+library(randomForest)
 library(gridExtra)
 library(forestError)
 
@@ -29,14 +29,30 @@ ui <- fluidPage(
                        min = 0, max = 1, value = 0)),
     column(3,
           sliderInput("c", h3("Constant Variance"),
-                      min = 0, max = 5, value = 0))
+                      min = 0, max = 5, value = 0)),
+      column(3, 
+             checkboxGroupInput("Assumptions", h3("Prediction Interval Assumptions"), 
+             choices = list("Linearity, Normality, and Constant Variance, " = "All", 
+                            "Symmetry and Constant Variance" = "Some", 
+                            "None" = "None"),
+             selected = 1)
+    )
+    
+  #  column(3, 
+  #         checkboxGroupInput(inputId = "disp", label = "Assumptions", h3("Checkbox group"), 
+  #         choices = list("Linear Structure, Normality, and Constant Variance" = 1, 
+  #                        "Symmetry and Constant Variance" = 2, 
+  #                        "None of these" = 3),
+  #         selected = 1)
+#)
 ),
  
 fluidRow(
   # Show a plot of the generated distribution
   mainPanel(
     plotOutput("plot"),
-    tableOutput("table")
+    tableOutput("table"),
+    textOutput("text")
   )  
 )
 
@@ -47,11 +63,11 @@ server <- function(input, output){
 
   Simulation <- function(a,b,c){
    
-   ntrain <- 10000
-   ntest <- 10000
+   ntrain <- 100
+   ntest <- 100
    alpha <- 0.1
    N <- ntrain + ntest
-   ns <- 2
+   ns <- 5
    
    
    #x1 <- rnorm(ntrain + ntest, 0, 1)
@@ -135,25 +151,37 @@ server <- function(input, output){
    rownames(Res_Table) <- c("Linear Model", "Random Forest with Symmetry Assumption", "Random Forest - No Symmetry Assumption", "Random Forest - No Constant Variance Assumption")
 
    #Plot <- qplot(Test$x1, Test$y)
-   p1<-ggplot(data=Test, aes(x=x1, y=y)) + geom_point(aes(color=LM_Contains)) + geom_line(aes(x=x1, y=LM_PI[,1]), color="red") + ylim(2*min(Test$y),2*max(Test$y) )
-   p1<-p1+geom_ribbon(aes(ymin=LM_PI[,2], ymax=LM_PI[,3]), linetype=2, alpha=0.5) + ggtitle("Assumes Lin., Norm., CV") + theme(legend.position = "none")
+  # p1<-ggplot(data=Test, aes(x=x1, y=y)) + geom_point(aes(color=LM_Contains)) + geom_line(aes(x=x1, y=LM_PI[,1]), color="red") + ylim(2*min(Test$y),2*max(Test$y) )
+   #p1<-p1+geom_ribbon(aes(ymin=LM_PI[,2], ymax=LM_PI[,3]), linetype=2, alpha=0.5, color="red", fill="lightblue") + ggtitle("Assumes Lin., Norm., CV") + theme(legend.position = "none")
    
-   p2<-ggplot(data=Test, aes(x=x1, y=y)) + geom_point(aes(color=RF2_Contains)) + geom_line(aes(x=x1, y=RF_PI2[,1]), color="red")+ ylim(2*min(Test$y),2*max(Test$y) )
-   p2<-p2+geom_ribbon(aes(ymin=RF_PI2[,2], ymax=RF_PI2[,3]), linetype=2, alpha=0.5) + ggtitle("Assumes Norm., CV") + theme(legend.position = "none")
+   #p2<-ggplot(data=Test, aes(x=x1, y=y)) + geom_point(aes(color=RF2_Contains)) + geom_line(aes(x=x1, y=RF_PI2[,1]), color="red")+ ylim(2*min(Test$y),2*max(Test$y) )
+   #p2<-p2+geom_ribbon(aes(ymin=RF_PI2[,2], ymax=RF_PI2[,3]), linetype=2, alpha=0.5) + ggtitle("Assumes Norm., CV") + theme(legend.position = "none")
    
-   p3<-ggplot(data=Test, aes(x=x1, y=y)) + geom_point(aes(color=RF_Contains)) + geom_line(aes(x=x1, y=RF_PI[,1]), color="red")+ ylim(2*min(Test$y),2*max(Test$y) )
-   p3<-p3+geom_ribbon(aes(ymin=RF_PI[,2], ymax=RF_PI[,3]), linetype=2, alpha=0.5)  + ggtitle("Assumes CV") + theme(legend.position = "none")
+   #p3<-ggplot(data=Test, aes(x=x1, y=y)) + geom_point(aes(color=RF_Contains)) + geom_line(aes(x=x1, y=RF_PI[,1]), color="red")+ ylim(2*min(Test$y),2*max(Test$y) )
+   #p3<-p3+geom_ribbon(aes(ymin=RF_PI[,2], ymax=RF_PI[,3]), linetype=2, alpha=0.5)  + ggtitle("Assumes CV") + theme(legend.position = "none")
    
-   p4<-ggplot(data=Test, aes(x=x1, y=y)) + geom_point(aes(color=QFE_Contains)) + geom_line(aes(x=x1, y=QFE_PI[,1]), color="red")+ ylim(2*min(Test$y),2*max(Test$y) )
-   p4<-p4+geom_ribbon(aes(ymin=QFE_PI[,2], ymax=QFE_PI[,3]), linetype=2, alpha=0.5)  + ggtitle("Assumes None of 3") + theme(legend.position = "none")
+   #p4<-ggplot(data=Test, aes(x=x1, y=y)) + geom_point(aes(color=QFE_Contains)) + geom_line(aes(x=x1, y=QFE_PI[,1]), color="red")+ ylim(2*min(Test$y),2*max(Test$y) )
+   #p4<-p4+geom_ribbon(aes(ymin=QFE_PI[,2], ymax=QFE_PI[,3]), linetype=2, alpha=0.5)  + ggtitle("Assumes None of 3") + theme(legend.position = "none")
    
-   Plots <- grid.arrange(p1, p2, p3, p4, ncol=2)
-   return(list(Plots, Res_Table))
+  #p <- p1 + geom_ribbon(aes(ymin=LM_PI[,2], ymax=LM_PI[,3]), linetype=2, alpha=0.5, color="grey", fill="lightgrey") + theme(legend.position = "none") + 
+  #          geom_ribbon(aes(ymin=RF_PI2[,2], ymax=RF_PI2[,3]), linetype=2, alpha=0.3, color="blue", fill="lightblue") + theme(legend.position = "none") + 
+  #          geom_ribbon(aes(ymin=QFE_PI[,2], ymax=QFE_PI[,3]), linetype=2, alpha=0.1, color="green", fill="lightgreen")  + theme(legend.position = "none")
+
+   text <- "Norm" %in% input$Assumptions
+   p <- ggplot(data=Test, aes(x=x1, y=y)) + geom_point() + geom_line(aes(x=x1, y=LM_PI[,1]), color="red") + ylim(1.5*min(Test$y),1.5*max(Test$y) )
+   p <- if("All" %in% input$Assumptions){p + geom_ribbon(aes(ymin=LM_PI[,2], ymax=LM_PI[,3]), linetype=2, alpha=0.5, color="grey", fill="grey")}else{p} 
+   p <- if("Some" %in% input$Assumptions){p + geom_ribbon(aes(ymin=RF_PI2[,2], ymax=RF_PI2[,3]), linetype=2, alpha=0.3, color="blue", fill="blue")}else{p}
+   p <- if("None" %in% input$Assumptions){p + geom_ribbon(aes(ymin=QFE_PI[,2], ymax=QFE_PI[,3]), linetype=2, alpha=0.5,  color="purple", fill="purple")}else{p}
+   p <- p + theme(legend.position = "blank")
+   
+   #Plots <- grid.arrange(p1, p2, p3, p4, ncol=2)
+  return(list(p, Res_Table, text))
    }
   
   SimulationRes <- reactive({Simulation(input$a,input$b,input$c)})
   output$plot <- renderPlot(SimulationRes()[[1]])
   output$table <- renderTable(SimulationRes()[[2]])
+  output$text <- renderText(SimulationRes()[[3]])
 }
 
 
