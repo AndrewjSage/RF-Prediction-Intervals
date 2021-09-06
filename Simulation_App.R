c978e15f8b0acbc93d16bd33565479e17f51d34d
andrewjsage <ajsage@gmail.com>
2021-09-06 18:53:16 -0500

    added regenerate button

--- Simulation_App.R
+++ Simulation_App.R
@@ -19,7 +19,7 @@ library(shinyWidgets)
 
 # Define UI ----
 ui <- fluidPage(
-  titlePanel("Random Forest Prediction Intervals"),
+  titlePanel("Prediction Intervals via Linear Model and Random Forest"),
   
   fluidRow(
     column(4, 
@@ -60,7 +60,7 @@ ui <- fluidPage(
 
           #sliderInput("c", h5("Constant Variance"),
           #            min = 0, max = 5, value = 0, ticks=FALSE)),
-   column(4, 
+   column(3, 
           selectInput("data", h5("Display"), 
                       choices = list("Training Data" = "Train", 
                                      "Test Data" = "Test"),
@@ -72,7 +72,7 @@ ui <- fluidPage(
                       min = -1, max = 1, step=0.05,
                       value = c(-1,1))
           ), 
-   column(4,
+   column(3,
            checkboxGroupInput("Estimate", h5("Display"), 
                               choices = list("True Expected Response Function" = "True",
                                              "Linear Model Estimate" = "LMest", 
@@ -84,8 +84,11 @@ ui <- fluidPage(
                                             "Symmetry and Constant Variance" = "Some", 
                                             "None" = "None"),
                              selected = 1)
-           )),
-  
+           ),
+   column(2,
+          actionButton("Regenerate", "Regenerate Data")
+   )
+  ),
 fluidRow(
   # Show a plot of the generated distribution
 #  mainPanel(
@@ -102,7 +105,9 @@ fluidRow(
 # Define server logic required to draw a histogram
 server <- function(input, output){
 
-  Simulation <- function(L,N,C, level){
+
+
+  Simulation <- function(L,N,C, action){
    
   a <- ifelse(L=="None",0, ifelse(L=="Slight", 2, 
                                ifelse(L=="Moderate", 4, ifelse(L=="Large", 6, 8))))
@@ -113,10 +118,10 @@ server <- function(input, output){
     
    ntrain <- 500
    ntest <- 500
-   alpha <- 1-level
+ #  alpha <- 1-level
    N <- ntrain + ntest
 #   ns <- min(ntrain/10, 10)
-   ns <- 15
+   ns <- 20
    
    
    #x1 <- rnorm(ntrain + ntest, 0, 1)
@@ -135,7 +140,7 @@ server <- function(input, output){
    x9 <- rnorm(ntrain + ntest, 0, 1)
    x10 <- rnorm(ntrain + ntest, 0, 1) 
    meanfunc <- function(x1){
-    mx <- x1 + a*x1^2 + a*x1^3*(a>3) + a*(x1>0)*(a>5) - a*(abs(x1)<0.5)*(a>7)
+    mx <- x1 + a*x1^2 + a*(x1-0.3)^5*(a>3) + a*(x1>0)*(a>5) - a*(abs(x1)<0.5)*(a>7)
   #   mx <- x1 + a*x1^2 + a*(x1^3)*(a>1.5)
   #   mx <- scale(mx) + a*(x1>0)*(a>3) 
      return(mx)
@@ -160,107 +165,90 @@ server <- function(input, output){
    y <- Train$y
    Testx <- Test[,1:10]
    y <- Test$y   
-   
-   
-   # Intervals from Linear Model
-   # Assume linearity, normality, constant variance
-   LM <- lm(data=Train, y~x1)
-   LMPred <- predict(LM, newdata=Test) 
-   MSPE_LM <- mean((LMPred-Test$y)^2)
-   LM_PI_Test <- predict(LM, newdata=Test, interval="prediction", level=1-alpha) 
-   LM_PI_Test <- data.frame(LM_PI_Test)
-   LM_Contains <- (Test$y >= LM_PI_Test[,2]) & (Test$y <= LM_PI_Test[,3])
-   Cov_LM <- mean(LM_Contains)
-   Width_LM <- mean(LM_PI_Test[,3]-LM_PI_Test[,2])
-   LM_PI_Test$x1 <- Test$x1
-   Test$LMPred <- LMPred
-   Test$LMLwr <- LM_PI_Test[,2] 
-   Test$LMUpr <- LM_PI_Test[,3]
-   Train$LMPred <- predict(LM, newdata=Train) 
-   LM_PI_Train <- predict(LM, newdata=Train, interval="prediction", level=1-alpha) 
-   Train$LMLwr <- LM_PI_Train[,2] 
-   Train$LMUpr <- LM_PI_Train[,3]
-  
-   Trainx <- data.frame(Trainx$x1)
+   Dataset <- rbind(Train, Test)
+   Trainx <- data.frame(Train$x1)
    names(Trainx) <- "x1"
-   Testx <- data.frame(Testx$x1)
+   Testx <- data.frame(Test$x1)
    names(Testx) <- "x1"
-   
-   
    RF <- randomForest(data=Train, x=Trainx, y=Train$y, nodesize=ns, method="forest", keep.inbag = TRUE)
-   RFPred <- predict(object=RF, newdata=Testx)
-   MSPE_RF <- mean((RFPred - Test$y)^2)
-   # Intervals from Random Forest - Sym. OOB method
-   # Assumes constant variance, error distribution symmetric
-   OOB_resid <- Train$y - RF$predicted
-   MOE <- quantile(abs(OOB_resid), 1-alpha)
-   RF_PI2_Test <- data.frame(RFPred, RFPred-MOE, RFPred+MOE)
-   RF2_Contains <- (Test$y >= RF_PI2_Test[,2]) & (Test$y <= RF_PI2_Test[,3])
-   Coverage_RFOOB_Sym <- mean(RF2_Contains)
-   Width_RFOOB_Sym <- mean(RF_PI2_Test[,3]-RF_PI2_Test[,2])
-   RF_PI2_Test$x1 <- Test$x1
-   Test$RFPred <- RFPred
-   Test$RF2Lwr <- RF_PI2_Test[,2] 
-   Test$RF2Upr <- RF_PI2_Test[,3]
-   Train$RFPred <- predict(RF, newdata=Train) 
-   Train$RF2Lwr <- Train$RFPred - MOE
-   Train$RF2Upr <- Train$RFPred + MOE
-   # Intervals from Random Forest - Non. Sym. OOB method
-   # Assumes constant variance
-   Lower <- quantile(OOB_resid, alpha/2)
-   Upper <- quantile(OOB_resid, 1-alpha/2)
-   RF_PI <- data.frame(RFPred, RFPred+Lower, RFPred+Upper)
-   RF_Contains <- (Test$y >= RF_PI[,2]) & (Test$y <= RF_PI[,3])
-   Coverage_RFOOB_NonSym <- mean(RF_Contains)
-   Width_RFOOB_NonSym <- mean(RF_PI[,3]-RF_PI[,2])
-   RF_PI$x1 <- Test$x1
-   Test$RFLwr <- RFPred+Lower
-   Test$RFUpr <- RFPred+Upper
-   Train$RFLwr <- Train$RFPred + Lower
-   Train$RFUpr <- Train$RFPred + Upper
-   library(forestError)
-   QFE <- quantForestError(forest=RF, X.train=Trainx, X.test=Testx, Y.train = Train$y,  alpha = alpha)
-   QFE2 <- quantForestError(forest=RF, X.train=Trainx, X.test=Trainx, Y.train = Train$y,  alpha = alpha)
-   Test$QFEPred <- QFE$estimates[,1]
-   Test$QFELwr <- QFE$estimates[,4]
-   Test$QFEUpr <- QFE$estimates[,5]
-   Train$QFEPred <- QFE2$estimates[,1]
-   Train$QFELwr <- QFE2$estimates[,4]
-   Train$QFEUpr <- QFE2$estimates[,5]
-   QFE_PI <- data.frame(Test$QFEPred, Test$QFELwr, Test$QFEUpr)
-   QFE_Contains <- (Test$y >= QFE_PI[,2]) & (Test$y <= QFE_PI[,3])
-   Coverage_QFE <- mean(QFE_Contains)
-   Width_QFE <- mean(QFE_PI[,3]-QFE_PI[,2])
-   QFE_PI$x1 <- Test$x1
    
-   #Res_LM <- c(MSPE_LM, Cov_LM, Width_LM)
-   #Res_RFOOBSym <- c(MSPE_RF, Coverage_RFOOB_Sym, Width_RFOOB_Sym)
-   #Res_RFOOBNonSym <- c(MSPE_RF, Coverage_RFOOB_NonSym, Width_RFOOB_NonSym)
-   #Res_QFE <- c(MSPE_RF, Coverage_QFE, Width_QFE)
+   return(list(Dataset, RF, action))
+  }
    
-   #Res_Table <- t(data.frame(Res_LM, Res_RFOOBSym, Res_RFOOBNonSym, Res_QFE))
-   #colnames(Res_Table) <- c("MSPE", "Coverage", "Width")
-   #rownames(Res_Table) <- c("Linear Model", "Random Forest with Symmetry Assumption", "Random Forest - No Symmetry Assumption", "Random Forest - No Constant Variance Assumption")
-
-   #Plot <- qplot(Test$x1, Test$y)
-  # p1<-ggplot(data=Test, aes(x=x1, y=y)) + geom_point(aes(color=LM_Contains)) + geom_line(aes(x=x1, y=LM_PI[,1]), color="red") + ylim(2*min(Test$y),2*max(Test$y) )
-   #p1<-p1+geom_ribbon(aes(ymin=LM_PI[,2], ymax=LM_PI[,3]), linetype=2, alpha=0.5, color="red", fill="lightblue") + ggtitle("Assumes Lin., Norm., CV") + theme(legend.position = "none")
    
-   #p2<-ggplot(data=Test, aes(x=x1, y=y)) + geom_point(aes(color=RF2_Contains)) + geom_line(aes(x=x1, y=RF_PI2[,1]), color="red")+ ylim(2*min(Test$y),2*max(Test$y) )
-   #p2<-p2+geom_ribbon(aes(ymin=RF_PI2[,2], ymax=RF_PI2[,3]), linetype=2, alpha=0.5) + ggtitle("Assumes Norm., CV") + theme(legend.position = "none")
-   
-   #p3<-ggplot(data=Test, aes(x=x1, y=y)) + geom_point(aes(color=RF_Contains)) + geom_line(aes(x=x1, y=RF_PI[,1]), color="red")+ ylim(2*min(Test$y),2*max(Test$y) )
-   #p3<-p3+geom_ribbon(aes(ymin=RF_PI[,2], ymax=RF_PI[,3]), linetype=2, alpha=0.5)  + ggtitle("Assumes CV") + theme(legend.position = "none")
-   
-   #p4<-ggplot(data=Test, aes(x=x1, y=y)) + geom_point(aes(color=QFE_Contains)) + geom_line(aes(x=x1, y=QFE_PI[,1]), color="red")+ ylim(2*min(Test$y),2*max(Test$y) )
-   #p4<-p4+geom_ribbon(aes(ymin=QFE_PI[,2], ymax=QFE_PI[,3]), linetype=2, alpha=0.5)  + ggtitle("Assumes None of 3") + theme(legend.position = "none")
-   
-  #p <- p1 + geom_ribbon(aes(ymin=LM_PI[,2], ymax=LM_PI[,3]), linetype=2, alpha=0.5, color="grey", fill="lightgrey") + theme(legend.position = "none") + 
-  #          geom_ribbon(aes(ymin=RF_PI2[,2], ymax=RF_PI2[,3]), linetype=2, alpha=0.3, color="blue", fill="lightblue") + theme(legend.position = "none") + 
-  #          geom_ribbon(aes(ymin=QFE_PI[,2], ymax=QFE_PI[,3]), linetype=2, alpha=0.1, color="green", fill="lightgreen")  + theme(legend.position = "none")
-
-   #text <- "Norm" %in% input$Assumptions
-  # text <- input$data
+  CalcPreds <- function(Dataset, RF, level){
+    # Intervals from Linear Model
+    # Assume linearity, normality, constant variance
+    alpha = 1-level
+    Train <- Dataset %>% filter(Datatype=="Train")
+    Test <- Dataset %>% filter(Datatype=="Test")
+    LM <- lm(data=Train, y~x1)
+    LMPred <- predict(LM, newdata=Test) 
+    MSPE_LM <- mean((LMPred-Test$y)^2)
+    LM_PI_Test <- predict(LM, newdata=Test, interval="prediction", level=1-alpha) 
+    LM_PI_Test <- data.frame(LM_PI_Test)
+    LM_Contains <- (Test$y >= LM_PI_Test[,2]) & (Test$y <= LM_PI_Test[,3])
+    Cov_LM <- mean(LM_Contains)
+    Width_LM <- mean(LM_PI_Test[,3]-LM_PI_Test[,2])
+    LM_PI_Test$x1 <- Test$x1
+    Test$LMPred <- LMPred
+    Test$LMLwr <- LM_PI_Test[,2] 
+    Test$LMUpr <- LM_PI_Test[,3]
+    Train$LMPred <- predict(LM, newdata=Train) 
+    LM_PI_Train <- predict(LM, newdata=Train, interval="prediction", level=1-alpha) 
+    Train$LMLwr <- LM_PI_Train[,2] 
+    Train$LMUpr <- LM_PI_Train[,3]
+    
+    Trainx <- data.frame(Train$x1)
+    names(Trainx) <- "x1"
+    Testx <- data.frame(Test$x1)
+    names(Testx) <- "x1"
+    
+         
+    RFPred <- predict(object=RF, newdata=Testx)
+    MSPE_RF <- mean((RFPred - Test$y)^2)
+    # Intervals from Random Forest - Sym. OOB method
+    # Assumes constant variance, error distribution symmetric
+    OOB_resid <- Train$y - RF$predicted
+    MOE <- quantile(abs(OOB_resid), 1-alpha)
+    RF_PI2_Test <- data.frame(RFPred, RFPred-MOE, RFPred+MOE)
+    RF2_Contains <- (Test$y >= RF_PI2_Test[,2]) & (Test$y <= RF_PI2_Test[,3])
+    Coverage_RFOOB_Sym <- mean(RF2_Contains)
+    Width_RFOOB_Sym <- mean(RF_PI2_Test[,3]-RF_PI2_Test[,2])
+    RF_PI2_Test$x1 <- Test$x1
+    Test$RFPred <- RFPred
+    Test$RF2Lwr <- RF_PI2_Test[,2] 
+    Test$RF2Upr <- RF_PI2_Test[,3]
+    Train$RFPred <- predict(RF, newdata=Train) 
+    Train$RF2Lwr <- Train$RFPred - MOE
+    Train$RF2Upr <- Train$RFPred + MOE
+    # Intervals from Random Forest - Non. Sym. OOB method
+    # Assumes constant variance
+    Lower <- quantile(OOB_resid, alpha/2)
+    Upper <- quantile(OOB_resid, 1-alpha/2)
+    RF_PI <- data.frame(RFPred, RFPred+Lower, RFPred+Upper)
+    RF_Contains <- (Test$y >= RF_PI[,2]) & (Test$y <= RF_PI[,3])
+    Coverage_RFOOB_NonSym <- mean(RF_Contains)
+    Width_RFOOB_NonSym <- mean(RF_PI[,3]-RF_PI[,2])
+    RF_PI$x1 <- Test$x1
+    Test$RFLwr <- RFPred+Lower
+    Test$RFUpr <- RFPred+Upper
+    Train$RFLwr <- Train$RFPred + Lower
+    Train$RFUpr <- Train$RFPred + Upper
+    QFE <- quantForestError(forest=RF, X.train=Trainx, X.test=Testx, Y.train = Train$y,  alpha = alpha)
+    QFE2 <- quantForestError(forest=RF, X.train=Trainx, X.test=Trainx, Y.train = Train$y,  alpha = alpha)
+    Test$QFEPred <- QFE$estimates[,1]
+    Test$QFELwr <- QFE$estimates[,4]
+    Test$QFEUpr <- QFE$estimates[,5]
+    Train$QFEPred <- QFE2$estimates[,1]
+    Train$QFELwr <- QFE2$estimates[,4]
+    Train$QFEUpr <- QFE2$estimates[,5]
+    QFE_PI <- data.frame(Test$QFEPred, Test$QFELwr, Test$QFEUpr)
+    QFE_Contains <- (Test$y >= QFE_PI[,2]) & (Test$y <= QFE_PI[,3])
+    Coverage_QFE <- mean(QFE_Contains)
+    Width_QFE <- mean(QFE_PI[,3]-QFE_PI[,2])
+    QFE_PI$x1 <- Test$x1
+    
    Dataset <- rbind(Train, Test)
    #return(list(Dataset, Res_Table))
    return(Dataset)
@@ -272,14 +260,14 @@ CreatePlot <- function(Dataset, data, Estimate, Assumptions, range){
 #   if(length(data)>1){Dataset <- rbind(Train, Test)}
    #if(length(input$data)>1){LM_PI <- rbind(LM_PI, LM_PI); RF_PI2 <- rbind(RF_PI2, RF_PI2); QFE_PI <- rbind(QFE_PI, QFE_PI)}
   Dataset <- Dataset %>% filter(x1 >= range[1] & x1 <= range[2]) 
-  p <- ggplot(data=Dataset, aes(x=x1, y=y)) + geom_point(aes(color=Datatype)) + ylim(2*min(Dataset$y),2*max(Dataset$y)) + theme_bw()
-  p <- if("True"%in% Estimate){p+geom_line(aes(x=x1, y=mx), color="green", size=3)}else{p}
-  p <- if("LMest"%in% Estimate){p+geom_line(aes(x=x1, y=LMPred), color="red")}else{p}
-  p <- if("RFest"%in% Estimate){p+geom_line(aes(x=x1, y=RFPred), color="blue")}else{p}
+  p <- ggplot(data=Dataset, aes(x=x1, y=y)) + geom_point(aes(color=Datatype)) + theme_bw() #+ ylim(2*min(Dataset$y),2*max(Dataset$y)) + theme_bw()
+  p <- if("True"%in% Estimate){p+geom_line(aes(x=x1, y=mx), color="black", size=2)}else{p}
+  p <- if("LMest"%in% Estimate){p+geom_line(aes(x=x1, y=LMPred), color="green", size=2)}else{p}
+  p <- if("RFest"%in% Estimate){p+geom_line(aes(x=x1, y=RFPred), color="blue", size=2)}else{p}
    p <- if("All" %in% Assumptions){p + geom_ribbon(aes(ymin=LMLwr, ymax=LMUpr), linetype=2, alpha=0.5, color="grey", fill="grey")}else{p} 
    p <- if("Some" %in% Assumptions){p + geom_ribbon(aes(ymin=RF2Lwr, ymax=RF2Upr), linetype=2, alpha=0.3, color="blue", fill="blue")}else{p}
    p <- if("None" %in% Assumptions){p + geom_ribbon(aes(ymin=QFELwr, ymax=QFEUpr), linetype=2, alpha=0.5,  color="purple", fill="purple")}else{p}
-   p <- p + theme(legend.position = "blank")
+  # p <- p + theme(legend.position = "blank")
   return(p)
    }   
   
@@ -311,12 +299,13 @@ EvaluatePI <- function(Dataset, range){
   return(c(PI_Table))
 }
 
-  SimulationRes <- reactive({Simulation(L=input$L,N=input$N,C=input$C, level=input$level)})
-  Plot <- reactive({CreatePlot(Dataset=SimulationRes(), data=input$data, Estimate=input$Estimate, Assumptions=input$Assumptions, range=input$range)})
+  SimulationRes <- reactive({Simulation(L=input$L,N=input$N,C=input$C, action=input$Regenerate)})
+  Preds <- reactive({CalcPreds(Dataset=SimulationRes()[[1]], RF=SimulationRes()[[2]], level=input$level)})
+  Plot <- reactive({CreatePlot(Dataset=Preds(), data=input$data, Estimate=input$Estimate, Assumptions=input$Assumptions, range=input$range)})
   output$plot <- renderPlot(Plot())
   #MSPE <- reactive(CalculateMSPE(Dataset=SimulationRes(), range=input$range))
-  output$MSPEtable <- renderTable(CalculateMSPE(Dataset=SimulationRes(), range=input$range), width="20%")
-  output$PItable <- renderTable(EvaluatePI(Dataset=SimulationRes(), range=input$range), width="20%")
+  output$MSPEtable <- renderTable(CalculateMSPE(Dataset=Preds(), range=input$range))
+  output$PItable <- renderTable(EvaluatePI(Dataset=Preds(), range=input$range))
     #output$text <- renderText(paste("Linear Model MSPE=",MSPE()[[1]], "Random Forest MSPE=", MSPE()[[2]]))
   #output$table <- renderTable(SimulationRes()[[2]])
   
