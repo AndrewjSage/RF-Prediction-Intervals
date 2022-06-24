07ebdfeda391500a128708bf45b9cdc9c725c4ed
andrewjsage <ajsage@gmail.com>
2022-06-24 15:50:36 -0500

    setup polynomial regression options

--- Prediction_Intervals_Simulation/app.R
+++ Prediction_Intervals_Simulation/app.R
@@ -60,17 +60,17 @@ ui <- fluidPage(
     #            min = 0, max = 5, value = 0, ticks=FALSE)),
     column(2, 
            
-           checkboxGroupInput("LMSettings", h5("Linear Model Settings"), 
-                              choices = list("Include Quadratic Term" = "Quad",
-                                             "Include Cubic Term" = "Cubic", 
-                                             "Use Log(Y) Transformation (negative Y values will be ignored)" = "Log"
+           radioButtons("LMSettings", h5("Degree of Regression Model"), 
+                              choices = list("Linear" = "Lin",
+                                             "Quadratic" = "Quad",
+                                             "Cubic" = "Cubic",
+                                             "Degree 4" = "Deg4",
+                                             "Degree 5" = "Deg5"
                               ),
            ),           
            
            
-           sliderInput("level", "Desired Coverage Level:",
-                       min = 0.7, max = 0.95, step=0.05,
-                       value = 0.9),
+           
            sliderInput("range", "Range:",
                        min = -1, max = 1, step=0.05,
                        value = c(-1,1))
@@ -88,31 +88,18 @@ ui <- fluidPage(
                                              "RF - most flexible -assumes none of these" = "None"),
                               selected = 1)
     ),
-    column(3,
-           #checkboxGroupInput("LinViol", h5("Type of Linearity Violation"), 
-          #                    choices = list("Exponential Trend" = "Exp",
-          #                                   "Polynomial Trend" = "Poly", 
-          #                                   "Discontinuity" = "Disc", 
-          #                                   "Unknown" = "Unknown"
-          #                    ),
-          # ),
-           radioButtons(inputId="LinViol", label="Type of Linearity Violation", 
-                        list("Exponential Trend" = "Exp",
-                             "Polynomial Trend" = "Poly", 
-                             "Discontinuity" = "Disc", 
-                             "Unknown" = "Unknown"
-                        ),
-           )
-           
-           ),
+    
     
     column(2,
            actionButton("Regenerate", "Regenerate Data"),
-           downloadButton("downloadData", "Download Data"),
+           sliderInput("level", "Desired Coverage Level:",
+                       min = 0.7, max = 0.95, step=0.05,
+                       value = 0.9),
            selectInput("data", h5("Display"), 
                        choices = list("Training Data" = "Train", 
                                       "Test Data" = "Test"),
                        selected = "Train"),
+           downloadButton("downloadData", "Download Data"),
            
     )
   ),
@@ -135,7 +122,7 @@ server <- function(input, output){
   
   
   
-  Simulation <- function(L,N,C, action, LinViol){
+  Simulation <- function(L,N,C, action){
     
     s1 <-1  
     s2 <-1  
@@ -147,11 +134,6 @@ server <- function(input, output){
                                         ifelse(N=="Moderate", 0.5, ifelse(N=="Large",0.75,1))))
     c <-  s3*ifelse(C=="None",0, ifelse(C=="Slight", 1, 
                                         ifelse(C=="Moderate", 2, ifelse(C=="Large", 3, 4))))
-    
-    a1 <- ifelse("Exp" %in% LinViol, 1, 0)
-    a2 <- ifelse("Poly" %in% LinViol, 1, 0)
-    a3 <- ifelse("Disc" %in% LinViol, 1, 0)
-    if("Unknown" %in% LinViol) {a1 <- a2 <- a3 <- 1}
     
     ntrain <- 1000
     ntest <- 1000
@@ -165,9 +147,7 @@ server <- function(input, output){
     x1 <- x1[sample(1:length(x1))]
     x1 <- x1[1:(ntrain+ntest)]
     meanfunc <- function(x1){
-      #mx <- a1*5*x1 + s2*a*x1^2 + a2*a*(x1-0.3)^5*(s1*a>1.5) + a3*a*(x1>0)*(s1*a>2.5) - a3*a*(abs(x1)<0.5)*(s1*a>3.5) + 20*a*(a1>0)*exp(1.1*x1)
-      
-      mx <- 10*(a1>0)*exp(0.2*a*x1) + a2*a*5*x1 + s2*a2*a*x1^2 + a2*a*(x1-0.3)^5*(s1*a>1.5) + 50*(a2>0 | a3>0 | a1 !=0) + a3*a*(x1>0)*(s1*a>2.5) - a3*a*(abs(x1)<0.5)*(s1*a>3.5)
+      mx <- 5*x1 + a*x1^2 + a*(x1-0.3)^5*(s1*a>1.5) + a*(x1>0)*(s1*a>2.5) - a*(abs(x1)<0.5)*(s1*a>3.5) 
       return(mx)
     }
     mx <- meanfunc(x1)
@@ -210,21 +190,15 @@ server <- function(input, output){
     if("Cubic"%in%LMSettings){
       LM <- lm(data=Train, y~x1 + I(x1^2) + I(x1^3))
     }
+    if("Deg4"%in%LMSettings){
+      LM <- lm(data=Train, y~x1 + I(x1^2) + I(x1^3) + I(x1^4))
+    }
+    if("Deg5"%in%LMSettings){
+      LM <- lm(data=Train, y~x1 + I(x1^2) + I(x1^3) + I(x1^4) + I(x1^5))
+    }
     LMPred <- predict(LM, newdata=Test)
     RMSPE_LM <- sqrt(mean((LMPred-Test$y)^2))
     LM_PI_Test <- predict(LM, newdata=Test, interval="prediction", level=1-alpha) 
-    if("Log"%in%LMSettings){
-      LM <- lm(data=Train, log(y)~x1)
-      if("Quad"%in%LMSettings){
-        LM <- lm(data=Train, log(y)~x1 + I(x1^2))
-      }
-      if("Cubic"%in%LMSettings){
-        LM <- lm(data=Train, log(y)~x1 + I(x1^2) + I(x1^3))
-      }
-      LMPred <- exp(predict(LM, newdata=Test))
-      RMSPE_LM <- sqrt(mean(LMPred-Test$y)^2)
-      LM_PI_Test <- exp(predict(LM, newdata=Test, interval="prediction", level=1-alpha)) 
-    }
     LM_PI_Test <- data.frame(LM_PI_Test)
     LM_Contains <- (Test$y >= LM_PI_Test[,2]) & (Test$y <= LM_PI_Test[,3])
     Cov_LM <- mean(LM_Contains)
@@ -237,11 +211,7 @@ server <- function(input, output){
     LM_PI_Train <- predict(LM, newdata=Train, interval="prediction", level=1-alpha) 
     Train$LMLwr <- LM_PI_Train[,2] 
     Train$LMUpr <- LM_PI_Train[,3]
-    if("Log"%in%LMSettings){
-      Train$LMPred <- exp(predict(LM, newdata=Train)) 
-      LM_PI_Train <- exp(predict(LM, newdata=Train, interval="prediction", level=1-alpha)) 
-      Train$LMLwr <- LM_PI_Train[,2] 
-      Train$LMUpr <- LM_PI_Train[,3]}
+
     
     Trainx <- data.frame(Train$x1)
     names(Trainx) <- "x1"
@@ -350,7 +320,7 @@ server <- function(input, output){
     return(c(PI_Table))
   }
   
-  SimulationRes <- reactive({Simulation(L=input$L,N=input$N,C=input$C, action=input$Regenerate, LinViol=input$LinViol)})
+  SimulationRes <- reactive({Simulation(L=input$L,N=input$N,C=input$C, action=input$Regenerate)})
   Preds <- reactive({CalcPreds(Dataset=SimulationRes()[[1]], RF=SimulationRes()[[2]], level=input$level, LMSettings = input$LMSettings)})
   Plot <- reactive({CreatePlot(Dataset=Preds(), data=input$data, Estimate=input$Estimate, Assumptions=input$Assumptions, range=input$range)})
   output$plot <- renderPlot(Plot())
